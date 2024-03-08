13ec99909af1e7bb5c7f18b46535d4a51018868a
mwafreaker <mwafreaker@gmail.com>
2024-03-08 11:38:47 +0300

    mod responsive

--- app/components/header/page.js
+++ app/components/header/page.js
@@ -5,13 +5,13 @@ import { GiHamburgerMenu } from "react-icons/gi"
 
 
 export default function Header() {
-    const [isOpen, setisOpen] = useState<Boolean>(false)
+    const [isOpen, setisOpen] = useState(false)
 
   function handleBurger() {
       setisOpen(!isOpen)
   }
     return (
-        <div className="flex flex-row justify-between px-10 py-4 pt-8">
+        <div className="flex flex-row justify-between px-10 py-4 pt-8 w-full">
           <strong>MARCHILLION</strong>
           <ul className="sm:flex flex-row gap-4 text-sm text-sky-600 hidden">
             <li>Home</li>
@@ -20,7 +20,7 @@ export default function Header() {
             <li>Blog</li>
             <li>Contact</li>
           </ul>
-          <GiHamburgerMenu className="sm:hidden cursor-pointer" onClick={handleBurger} />
+          <GiHamburgerMenu className="sm:hidden cursor-pointer scale-150" onClick={handleBurger} />
         </div>
     )
 }
--- app/page.js
+++ app/page.js
@@ -4,18 +4,18 @@ import Header from "./components/header/page";
 export default function Home() {
   return (
     <div className="min-h-screen">
-      <section>
+      <section className="absolute top-0 left-0 z-10 w-screen">
         <Header />
       </section>
 
-      <section className="flex flex-col pl-8 pt-96 h-3/5 gap-4 align-bottom">
-        <div className="flex flex-row">
+      <section className="flex flex-col pl-8 pt-96 h-screen md:h-3/5 gap-4 align-bottom bg-[url('./utils/img/rental1.jpeg')]">
+        <div className="flex flex-row bottom-0 left-0">
           <div className="flex flex-col gap-1 w-3/4">
           <p className="text-3xl flex flex-col justify-center align-middle font-semibold">
             Book your hotel with us
           </p>
           <span className="text-xs text-gray-700">
-            Find vacation rentals, cabins,beach houses, unique homes and experiences around the world. 
+            Find vacation rentals, cabins,beach houses, unique homes and experiences from around the world. 
           </span>
           </div>
         </div>
